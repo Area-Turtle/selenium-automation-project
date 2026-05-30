d01367ecb786f4fb1bb285c09b04ebfc9e53c436
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-05-30 03:07:22 -0700

    update test | update yml

--- seleneium_smoketest.js
+++ seleneium_smoketest.js
@@ -1,4 +1,4 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 async function runSmokeTest() {
