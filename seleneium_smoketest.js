4aa47e1d45b2ad51971b7aed41fb58da5cef994a
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-05-30 03:02:06 -0700

    update test

--- seleneium_smoketest.js
+++ seleneium_smoketest.js
@@ -2,7 +2,7 @@ const { Builder } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 async function runSmokeTest() {
-     let options = new chrome.Options();
+    let options = new chrome.Options();
     options.addArguments('--headless');
     options.addArguments('--no-sandbox');
     options.addArguments('--disable-dev-shm-usage');
@@ -15,10 +15,43 @@ async function runSmokeTest() {
     try {
         await driver.get('http://localhost:9292/');
         let title = await driver.getTitle();
-        console.log("Page Title:", title);
+        // console.log("Page Title:", title);
 
-        if (!title) {
-            throw new Error("Localhost did not load properly.");
+        if (title !== 'The Internet') {
+            throw new Error(
+                `Expected title "The Internet" but found "${title}"`
+                
+            );
+        }
+
+    } finally {
+        await driver.quit();
+    }
+}
+
+async function runFindPageTitle() {
+    let options = new chrome.Options();
+    options.addArguments('--headless');
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-dev-shm-usage');
+
+    let driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .build();
+
+    try {
+        await driver.get('http://localhost:9292/');
+        let headTitle = await driver.getFindElement(
+            By.Css("h1.heading")
+        ).getText();
+        console.log("heading:", headTitle);
+
+        if (headTitle !== 'Welcome to the-internet') {
+            throw new Error(
+                `Expected title "Welcome to the-internet" but found "${title}"`
+                
+            );
         }
 
     } finally {
@@ -27,3 +60,4 @@ async function runSmokeTest() {
 }
 
 runSmokeTest();
+runFindPageTitle()
