e4065465a160a9a2745e6101f7e9c8ef5b3b50f5
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-06-02 02:31:57 -0700

    update test

---
+++ seleneium_tests/AddRemoveElements_Test.js
@@ -0,0 +1,60 @@
+const { Builder, By } = require('selenium-webdriver');
+const { createDriver, validate, waitForVisible, addSummary } = require('../support/commonActions');
+const BASE_URL = 'http://localhost:9292/';
+const elementCount = 1
+
+async function runARMainTest(driver) {
+    await driver.get(BASE_URL);
+    <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText('A/B Testing')).click();
+
+    const title = await driver.getTitle();
+    console.log("Add/Remove Elements Head Title:", title);
+
+    // test validate: (name, actual, and expected)
+    return validate(
+        'Add/Remove Elements Head Title',
+        title,
+        'The Internet'
+    );
+}
+async function runARFindPageHeading(driver) {
+    const element = await waitForVisible(driver, By.css('h3.heading'));
+    const heading = await element.getText();
+
+    console.log("H3 Heading:", heading);
+
+    // test validate: (name, actual, and expected)
+    return validate(
+        'H3 Heading Test',
+        heading,
+        'Add/Remove Elements'
+    );
+}
+async function runARAddElement(driver) {
+
+    const element = await waitForVisible(driver, By.css('h3.heading'));
+    const heading = await element.getText();
+
+    console.log("H3 Heading:", heading);
+
+    // test validate: (name, actual, and expected)
+    return validate(
+        'H3 Heading Test',
+        heading,
+        'Add/Remove Elements'
+    );
+}
+async function runARRemoveElement(driver) {
+    const element = await waitForVisible(driver, By.css('h3.heading'));
+    const heading = await element.getText();
+
+    console.log("H3 Heading:", heading);
+
+    // test validate: (name, actual, and expected)
+    return validate(
+        'H3 Heading Test',
+        heading,
+        'Add/Remove Elements'
+    );
+}
