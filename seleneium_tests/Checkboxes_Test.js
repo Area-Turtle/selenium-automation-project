250485cfe0c077774aa97753848d659f1400aec4
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-06-10 00:44:18 -0700

    update test

--- seleneium_tests/Checkboxes_Test.js
+++ seleneium_tests/Checkboxes_Test.js
@@ -34,3 +34,57 @@ async function runCBFindPageHeading(driver) {
         'Checkboxes'
     );
 }
+
+async function runCBCheckboxCheck(driver) {
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText('Checkboxes')).click();
+    const checkboxes = await driver.findElements(
+        By.css('#checkboxes input[type="checkbox"]')
+    );
+
+    if (checkboxes.length !== 2) {
+        throw new Error(`Expected 2 checkboxes, found ${checkboxes.length}`);
+    }
+
+    // Verify initial state
+    const checkbox1Checked = await checkboxes[0].isSelected();
+    const checkbox2Checked = await checkboxes[1].isSelected();
+
+    // console.log('Checkbox 1:', checkbox1Checked);
+    // console.log('Checkbox 2:', checkbox2Checked);
+
+    if (checkbox1Checked) {
+        // throw new Error('Checkbox 1 should be unchecked by default');
+        console.log('Checkbox 1:', checkboxes[0]);
+    }
+
+    if (!checkbox2Checked) {
+        // throw new Error('Checkbox 2 should be checked by default');
+        console.log('Checkbox 2:', checkboxes[1]);
+    }
+
+    // Toggle checkbox 1 ON
+    await checkboxes[0].click();
+
+    if (!(await checkboxes[0].isSelected())) {
+        // throw new Error('Checkbox 1 should be checked after click');
+        console.log('Checkbox 1:', checkboxes[0]);
+    }
+
+    // Toggle checkbox 2 OFF
+    await checkboxes[1].click();
+
+    if (await checkboxes[1].isSelected()) {
+        // throw new Error('Checkbox 2 should be unchecked after click');
+        console.log('Checkbox 2 status:', checkboxes[1]);
+    }
+
+    console.log('Checkbox test passed');
+    // test validate: (name, actual, and expected)
+    return commonActions.validate(
+        'Checkbox State Test',
+        checkbox0 === true && checkbox1 === false,
+        true
+    );
+}
