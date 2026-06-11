8e9c0206ca3e6977a067f55550947caa42088e37
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-06-11 01:48:49 -0700

    update test

--- seleneium_tests/DynamicControls_Test.js
+++ seleneium_tests/DynamicControls_Test.js
@@ -20,9 +20,9 @@ async function runDC2MainTest(driver) {
     );
 }
 async function runDC2FindPageHeading(driver) {
-        await driver.get(BASE_URL);
-        // <a href="/add_remove_elements/">Add/Remove Elements</a>
-        await driver.findElement(By.linkText(pageTopic)).click();
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
     const element = await commonActions.waitForVisible(driver, By.css('.example h3'));
     const heading = await element.getText();
 
@@ -33,5 +33,109 @@ async function runDC2FindPageHeading(driver) {
         `${pageTopic} H3 Heading Test`,
         heading,
         pageTopic
+    );
+}
+
+async function runDC2CheckBoxRemoveAdd(driver) {
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
+
+
+    const checkbox = await driver.findElement(
+        By.css('#checkbox input[type="checkbox"]')
+    );
+
+    await driver.findElement(
+        By.css('#checkbox-example button')
+    ).click();
+
+    await driver.wait(
+        until.stalenessOf(checkbox),
+        10000
+    );
+
+    const message = await driver.findElement(
+        By.id('message')
+    ).getText();
+
+    // message should be: It's gone!
+    console.log(rmessage);
+
+    const removedSuccessfully = message === "It's gone!";
+    
+    await driver.findElement(
+        By.css('#checkbox-example button')
+    ).click();
+
+    await driver.wait(
+        until.elementLocated(
+            By.css('#checkbox input[type="checkbox"]')
+        ),
+        10000
+    );
+
+    const checkboxExists =
+        (await driver.findElements(
+            By.css('#checkbox input[type="checkbox"]')
+        )).length > 0;
+
+
+    return commonActions.validate(
+        `(${pageTopic}) Checkbox Remove/Add Test`,
+        removedSuccessfully && checkboxExists,
+        true
+    );
+}
+
+async function runDC2InputDisableEnable(driver) {
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
+
+    const input = await driver.findElement(
+        By.css('#input-example input')
+    );
+
+    const enabled = await input.isEnabled();
+
+    console.log(enabled);
+
+    await driver.findElement(
+        By.css('#input-example button')
+    ).click();
+
+    await driver.wait(async () => {
+        return await input.isEnabled();
+    }, 10000);
+
+    const inputEnabled = await driver.findElement(
+        By.id('message')
+    ).getText();
+
+    console.log(inputEnabled)
+
+    await driver.findElement(
+        By.css('#input-example button')
+    ).click();
+
+    await driver.wait(async () => {
+        return !(await input.isEnabled());
+    }, 10000);
+
+    const inputDisabled = await driver.findElement(
+        By.id('message')
+    ).getText();
+
+    console.log(inputDisabled)
+
+    const enabledCorrect = inputEnabled === "It's enabled!";
+    const disabledCorrect = inputDisabled === "It's disabled!"; 
+
+    // test validate: (name, actual, and expected)
+    return commonActions.validate(
+        `(${pageTopic}) InputDisabledEnabled Test`,
+        enabledCorrect && disabledCorrect,
+        true
     );
 }
