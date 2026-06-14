d7086276809d3fba44d5b59d8207c069932ff758
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-06-14 15:46:28 -0700

    update test

--- seleneium_tests/DynamicLoading_Test.js
+++ seleneium_tests/DynamicLoading_Test.js
@@ -20,9 +20,9 @@ async function runDLMainTest(driver) {
     );
 }
 async function runDLFindPageHeading(driver) {
-        await driver.get(BASE_URL);
-        // <a href="/add_remove_elements/">Add/Remove Elements</a>
-        await driver.findElement(By.linkText(pageTopic)).click();
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
     const element = await commonActions.waitForVisible(driver, By.css('.example h3'));
     const heading = await element.getText();
 
@@ -33,5 +33,58 @@ async function runDLFindPageHeading(driver) {
         `${pageTopic} H3 Heading Test`,
         heading,
         pageTopic
+    );
+}
+
+async function runDLDynamicExample1(driver) {
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
+
+    await driver.findElement(
+        By.linkText('Example 1: Element on page that is hidden')
+    ).click();
+
+    await driver.findElement(By.css('#start button')).click();
+
+    const hello = await driver.wait(
+        until.elementLocated(By.css('#finish h4')),
+        10000
+    );
+
+    await driver.wait(
+        until.elementIsVisible(hello),
+        10000
+    );
+
+    const text = await hello.getText();
+
+    return commonActions.validate(
+        'Dynamic Loading Example 1',
+        text,
+        'Hello World!'
+    );
+}
+
+async function runDLExample2(driver) {
+    await driver.get(BASE_URL);
+    await driver.findElement(By.linkText('Dynamic Loading')).click();
+    await driver.findElement(
+        By.linkText('Example 2: Element rendered after the fact')
+    ).click();
+
+    await driver.findElement(By.css('#start button')).click();
+
+    const hello = await driver.wait(
+        until.elementLocated(By.css('#finish h4')),
+        10000
+    );
+
+    const text = await hello.getText();
+
+    return commonActions.validate(
+        'Dynamic Loading Example 2',
+        text,
+        'Hello World!'
     );
 }
