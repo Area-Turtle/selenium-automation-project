313821eba85332dfdaaf2b20c86ccbffc57fa3de
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-06-16 01:08:50 -0700

    update test

--- seleneium_tests/EntryAd_Test.js
+++ seleneium_tests/EntryAd_Test.js
@@ -3,11 +3,27 @@ const commonActions = require('../support/commonActions');
 const BASE_URL = 'http://localhost:9292/';
 const pageTopic = 'Entry Ad';
 
+function closeModal() {
+    await driver.findElement(
+        By.css('.modal-footer p')
+    ).click();
+
+    await driver.wait(async () => {
+        const modals = await driver.findElements(
+            By.css('.modal')
+        );
+
+        return modals.length === 0 ||
+            !(await modals[0].isDisplayed());
+    }, 10000);
+}
 
 async function runEAMainTest(driver) {
     await driver.get(BASE_URL);
     // <a href="/add_remove_elements/">Add/Remove Elements</a>
     await driver.findElement(By.linkText(pageTopic)).click();
+    closeModal()
+
 
     const title = await driver.getTitle();
     console.log(`(${pageTopic}) Head Title:`, title);
@@ -20,9 +36,10 @@ async function runEAMainTest(driver) {
     );
 }
 async function runEAFindPageHeading(driver) {
-        await driver.get(BASE_URL);
-        // <a href="/add_remove_elements/">Add/Remove Elements</a>
-        await driver.findElement(By.linkText(pageTopic)).click();
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
+    closeModal()
     const element = await commonActions.waitForVisible(driver, By.css('.example h3'));
     const heading = await element.getText();
 
@@ -33,5 +50,44 @@ async function runEAFindPageHeading(driver) {
         `${pageTopic} H3 Heading Test`,
         heading,
         pageTopic
+    );
+}
+
+async function runEAVerifiyModal(driver) {
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
+
+    const title = await driver.findElement(
+        By.css('.modal h3')
+    ).getText();
+
+    return commonActions.validate(
+        'Modal Title',
+        title.toUpperCase(),
+        'THIS IS A MODAL WINDOW'
+    );
+}
+
+async function runEAReOpenModal(driver) {
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
+
+    closeModal()
+
+    await driver.findElement(
+        By.linkText('click here')
+    ).click();
+
+    const modal = await driver.wait(
+        until.elementLocated(By.css('.modal')),
+        10000
+    );
+
+    return commonActions.validate(
+        'Modal Reopened',
+        await modal.isDisplayed(),
+        true
     );
 }
