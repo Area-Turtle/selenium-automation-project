18207b4043603caf921d48c1026644ad156358e1
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-06-17 03:15:03 -0700

    update test

---
+++ seleneium_tests/JavaScriptAlerts_Test.js
@@ -0,0 +1,37 @@
+const { Builder, By } = require('selenium-webdriver');
+const commonActions = require('../support/commonActions');
+const BASE_URL = 'http://localhost:9292/';
+const pageTopic = 'JavaScript Alerts';
+
+
+async function runIMainTest(driver) {
+    await driver.get(BASE_URL);
+    // <a href="/add_remove_elements/">Add/Remove Elements</a>
+    await driver.findElement(By.linkText(pageTopic)).click();
+
+    const title = await driver.getTitle();
+    console.log(`(${pageTopic}) Head Title:`, title);
+
+    // test validate: (name, actual, and expected)
+    return commonActions.validate(
+        `(${pageTopic}) Head Title`,
+        title,
+        'The Internet'
+    );
+}
+async function runIFindPageHeading(driver) {
+        await driver.get(BASE_URL);
+        // <a href="/add_remove_elements/">Add/Remove Elements</a>
+        await driver.findElement(By.linkText(pageTopic)).click();
+    const element = await commonActions.waitForVisible(driver, By.css('.example h3'));
+    const heading = await element.getText();
+
+    console.log("H3 Heading:", heading);
+
+    // test validate: (name, actual, and expected)
+    return commonActions.validate(
+        `${pageTopic} H3 Heading Test`,
+        heading,
+        pageTopic
+    );
+}
